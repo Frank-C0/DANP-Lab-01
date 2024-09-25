2cacfc3286fdcdbcd6ba11abb46093daccab210e
danp <developer@dnap.com>
2024-09-25 09:07:17 -0500

    Initial Commit

---
+++ Login Example/Login_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_ExampleApp.swift
+//  Login Example
+//
+//  Created by epismac on 25/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Login_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
